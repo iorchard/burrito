4e55247cfcbfafc1f70e799b9d5dcb6c5d81ed59
Heechul Kim <jijisa@iorchard.net>
2022-12-15 23:14:25 +0900

    add image_push.yml playbook files

---
+++ scripts/image_push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+for src in $(cat images.txt); do
+  repo=${src#*/}
+  dst="${LOCAL_REGISTRY}/${repo}"
+  echo "Pull ${src}"
+  nerdctl pull $src &> /dev/null
+  echo "Tag ${src} to ${dst}"
+  nerdctl tag $src $dst
+  echo "Push ${dst}"
+  nerdctl --insecure-registry push $dst &> /dev/null
+done
