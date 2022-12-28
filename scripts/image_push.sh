ad1a0bab7f372fe2f090b860a5ddaf7a866ab363
Heechul Kim <jijisa@iorchard.net>
2022-12-28 18:07:04 +0900

    updated openstack images to customized images; include PyNacl package to support mariadb ed25519 auth plugin

--- scripts/image_push.sh
+++ scripts/image_push.sh
@@ -5,10 +5,13 @@ set -e
 for src in $(cat images.txt); do
   repo=${src#*/}
   dst="${LOCAL_REGISTRY}/${repo}"
-  echo "Pull ${src}"
-  nerdctl pull $src &> /dev/null
-  echo "Tag ${src} to ${dst}"
-  nerdctl tag $src $dst
-  echo "Push ${dst}"
-  nerdctl --insecure-registry push $dst &> /dev/null
+  #echo "${src} => ${dst}"
+  echo "== Pull ${src}"
+  #nerdctl pull ${src} &> /dev/null
+  nerdctl pull ${src}
+  echo "== Tag ${src} to ${dst}"
+  nerdctl tag ${src} ${dst}
+  echo "== Push ${dst}"
+  #nerdctl --insecure-registry push ${dst} &> /dev/null
+  nerdctl --insecure-registry push ${dst}
 done
