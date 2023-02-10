5c098b367c9514fd5a92ddc21d5bd4e1a387ab5c
Heechul Kim <jijisa@iorchard.net>
2023-02-10 11:53:06 +0900

    support both online and offline image push to registry

--- scripts/registry.sh
+++ scripts/registry.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-set -e
+echo "local registry: ${LOCAL_REGISTRY}"
+echo "seed registry: ${SEED_REGISTRY}"
+echo "offline: ${OFFLINE}"
 
 for src in $(cat images.txt); do
+  if [ "${OFFLINE}" = "1" ]; then
+    registry=${src%%/*}
+    src=${src/${registry}/${SEED_REGISTRY}}
+  fi
   repo=${src#*/}
   dst="${LOCAL_REGISTRY}/${repo}"
-  #echo "${src} => ${dst}"
+  echo "${src} => ${dst}"
   echo "== Pull ${src}"
-  #nerdctl pull ${src} &> /dev/null
-  nerdctl pull ${src}
+  nerdctl pull ${src} &> /dev/null
   echo "== Tag ${src} to ${dst}"
   nerdctl tag ${src} ${dst}
   echo "== Push ${dst}"
-  #nerdctl --insecure-registry push ${dst} &> /dev/null
-  nerdctl --insecure-registry push ${dst}
+  nerdctl --insecure-registry push ${dst} &> /dev/null
+  echo "== Delete ${src} and ${dst}"
+  nerdctl rmi ${src} ${dst}
 done
