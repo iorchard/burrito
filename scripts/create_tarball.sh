db753a83f3f0665a89abffd9a2ff9196c0c3901a
Heechul Kim <jijisa@iorchard.net>
2023-02-10 19:55:12 +0900

    add create_tarball.sh script

---
+++ scripts/create_tarball.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+GIT_TAG="${1:-1.0.0}"
+
+CURRENT_DIR=$( dirname "$(readlink -f "$0")" )
+DIST_DIR=${CURRENT_DIR}/dist
+
+curl -sLo ${CURRENT_DIR}/git-archive-all.sh \
+  https://raw.githubusercontent.com/fabacab/git-archive-all.sh/master/git-archive-all.sh
+chmod +x ${CURRENT_DIR}/git-archive-all.sh
+
+git clone --recursive -b ${GIT_TAG} https://github.com/iorchard/burrito.git \
+  ${DIST_DIR}/burrito
+pushd ${DIST_DIR}/burrito
+  ${CURRENT_DIR}/git-archive-all.sh -- - | \
+    gzip -9 > ${DIST_DIR}/burrito-${GIT_TAG}.tar.gz
+popd
+rm -fr ${DIST_DIR}/burrito
