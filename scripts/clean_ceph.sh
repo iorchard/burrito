d4d828f40501e69000154d03a2437d251be2f99a
Heechul Kim <jijisa@iorchard.net>
2023-05-14 11:48:59 +0900

    feature: add ceph purge script

---
+++ scripts/clean_ceph.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+CURRENT_DIR=$( dirname "$(readlink -f "$0")" )
+
+source ~/.envs/burrito/bin/activate
+OFFLINE_VARS=
+pushd ${CURRENT_DIR}/../
+  if [ -f .offline_flag ] && \
+    ${CURRENT_DIR}/offline_services.sh -s &>/dev/null; then
+    OFFLINE_VARS="--extra-vars=@offline_vars.yml"
+  fi
+  ansible-playbook --extra-vars=@vars.yml ${OFFLINE_VARS} \
+    ceph-ansible/infrastructure-playbooks/purge-cluster.yml
+popd
