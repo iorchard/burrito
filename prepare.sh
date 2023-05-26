e57123120d599676277956a3f2091681fe423d58
Heechul Kim <jijisa@iorchard.net>
2023-05-26 17:33:51 +0900

    feature: update prepare.sh to include all preparation tasks

--- prepare.sh
+++ prepare.sh
@@ -3,21 +3,35 @@
 set -e 
 
 OPT=${1:-"online"}
+CFGFILES=(ansible.cfg hosts vars.yml)
 
-if [ x"$OPT" = x"offline" ]; then 
+if [ x"$OPT" = x"offline" ]; then
+  ./scripts/offline_services.sh --up
+  sudo dnf -y install python39
+  python3.9 -m venv ~/.envs/burrito
+  . ~/.envs/burrito/bin/activate
+  pip install --no-index --find-links /mnt/pypi /mnt/pypi/{pip,wheel}-*
+  pip install --no-index --find-links /mnt/pypi \
+               --requirement requirements.txt
   pushd /mnt/ansible_collections
     ansible-galaxy install --force -r requirements.yml
   popd
 else
+  sudo dnf -y install git python3 python39 python3-cryptography epel-release
+  python3.9 -m venv ~/.envs/burrito
+  . ~/.envs/burrito/bin/activate
   python -m pip install -U pip
   python -m pip install wheel
   python -m pip install -r requirements.txt
   ansible-galaxy install -r ceph-ansible/requirements.yml
 fi
 
-cp ansible.cfg.sample ansible.cfg
-cp hosts.sample hosts
-cp vars.yml.sample vars.yml
+for CFG in ${CFGFILES[@]};do
+  if [ -f "${CFG}" ]; then
+    echo mv ${CFG} ${CFG}.$(date --iso-8601='seconds')
+  fi
+  cp ${CFG}.sample ${CFG}
+done
 
 mkdir -p group_vars/all
 cp ceph_vars.yml.tpl group_vars/all/ceph_vars.yml
