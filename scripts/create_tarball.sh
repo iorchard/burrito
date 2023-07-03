57144a4b171eb7361d7ae389f55856de3f784ba7
Heechul Kim <jijisa@iorchard.net>
2023-07-03 09:59:02 +0900

    feature: include tag and commit id info in release tarball

--- scripts/create_tarball.sh
+++ scripts/create_tarball.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-GIT_TAG="${1:-1.0.0}"
+SRC_VER="${1:-1.0.0}"
+REL_NAME="${SRC_VER//\//_}"
 
 function USAGE() {
   echo "USAGE: $0 <git_tag>" 1>&2
@@ -23,10 +24,16 @@ curl -sLo ${CURRENT_DIR}/git-archive-all.sh \
   https://raw.githubusercontent.com/fabacab/git-archive-all.sh/master/git-archive-all.sh
 chmod +x ${CURRENT_DIR}/git-archive-all.sh
 
-git clone --recursive -b ${GIT_TAG} https://github.com/iorchard/burrito.git \
+git clone --recursive -b ${SRC_VER} https://github.com/iorchard/burrito.git \
   ${DIST_DIR}/burrito
 pushd ${DIST_DIR}/burrito
-  ${CURRENT_DIR}/git-archive-all.sh --prefix burrito-${GIT_TAG}/ -- - | \
-    gzip -9 > ${DIST_DIR}/burrito-${GIT_TAG}.tar.gz
+  echo ${SRC_VER} \($(git rev-parse HEAD)\) > VERSION
+  ${CURRENT_DIR}/git-archive-all.sh --prefix burrito-${REL_NAME}/ \
+    ${DIST_DIR}/burrito-${REL_NAME}.tar
+  # add VERSION to tarball
+  tar --xform="s#^#burrito-${REL_NAME}/#" -rf \
+    ${DIST_DIR}/burrito-${REL_NAME}.tar VERSION
+  # compress
+  gzip -9f ${DIST_DIR}/burrito-${REL_NAME}.tar
 popd
 rm -fr ${DIST_DIR}/burrito
