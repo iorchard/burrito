7fd2859a11c8715d15b4cd1e20b8b65bd6af9e0d
Heechul Kim <jijisa@iorchard.co.kr>
2025-06-02 20:10:21 +0900

    patch: include 02, 03, 04 patch files

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -7,7 +7,7 @@ REG_TMPL_DIR="kubespray/roles/kubernetes-apps/registry/templates"
 
 if [ "x${ACTION}" = "x-r" ]; then
   rm -f ${REG_TMPL_DIR}/registry-certs.yml.j2
-  for p in patches/09-patch*.txt patches/1[0,2,7,8]-patch*.txt; do
+  for p in patches/0[2-4,9]-patch*.txt patches/1[0,2,7,8]-patch*.txt; do
     if patch -sfRp0 --dry-run <${p} >/dev/null; then
       patch -Rp0 <${p}
     else
@@ -17,7 +17,7 @@ if [ "x${ACTION}" = "x-r" ]; then
 else
   cp -f patches/${REG_TMPL_DIR}/registry-certs.yml.j2 \
     ${REG_TMPL_DIR}/registry-certs.yml.j2
-  for p in patches/09-patch*.txt patches/1[0,2,7,8]-patch*.txt; do
+  for p in patches/0[2-4,9]-patch*.txt patches/1[0,2,7,8]-patch*.txt; do
     if patch -sNp0 --dry-run <${p} >/dev/null; then
       patch -Np0 <${p}
     else
