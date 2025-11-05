ce93960b3e4326e2631cbbd6f5ef701a0a8e6cd5
Heechul Kim <jijisa@iorchard.co.kr>
2025-11-05 23:32:59 +0900

    include leap_vars.yml when running k8s playbook

--- run.sh
+++ run.sh
@@ -13,20 +13,11 @@ function USAGE() {
   echo "ping      - ping to all nodes."
   echo "preflight - play common tasks, i.e. yum repo settings."
   echo "ha        - play ha stack tasks, HAProxy/KeepAlived."
-  echo "ceph      - play ceph tasks. (when ceph in storage backend)"
   echo "k8s       - play kubernetes installation tasks."
-  echo "netapp    - play netapp tasks. (when netapp in storage backend)"
-  echo "powerflex - play powerflex tasks. (when powerflex in storage backend)"
-  echo "hitachi   - play hitachi tasks. (when hitachi in storage backend)"
-  echo "patch     - play kubernetes security patch tasks."
-  echo "registry  - play local registry setup tasks. (offline install only)"
-  echo "landing   - play localrepo/genesis registry setup tasks."
-  echo "burrito   - play openstack installation tasks."
-  echo "scale     - play kubernetes node addition tasks."
   echo
   echo "ansible_parameters"
   echo "=================="
-  echo "ex) --tags=ceph-csi"
+  echo "ex) --tags=some_tag"
   echo 
 }
 if [ $# -lt 1 ]; then
@@ -38,6 +29,7 @@ PLAYBOOK=$1
 shift
 
 OFFLINE_VARS=
+LEAP_VARS="leap_vars.yml"
 
 if [[ "${PLAYBOOK}" = "vault" ]]; then
   . ~/.envs/burrito/bin/activate
@@ -60,22 +52,14 @@ if [ -f .offline_flag ]; then
     exit 1
   fi
 fi
-[[ "${PLAYBOOK}" = "k8s" || "${PLAYBOOK}" = "scale" ]] && FLAGS="-b" || FLAGS=
+if [ "${PLAYBOOK}" = "k8s" -a ! -f ${LEAP_VARS} ]; then
+  echo "${LEAP_VARS} is not found. Did you run the leap playbook?"
+  exit 1
+fi
+[[ "${PLAYBOOK}" = "k8s" ]] && FLAGS="-b --extra-vars=@${LEAP_VARS}" || FLAGS=
 FLAGS="${FLAGS} $@"
 
-[[ "${PLAYBOOK}" = "scale" ]] && PLAYBOOK="kubespray/${PLAYBOOK}" || :
 [[ "${PLAYBOOK}" = "k8s" ]] && PLAYBOOK="kubespray/cluster" || :
-
-if [[ "${PLAYBOOK}" = "burrito" && -n ${OFFLINE_VARS} ]]; then
-  if ! (helm plugin list | grep -q ^diff); then
-    # install helm diff plugin
-    HELM_DIFF_TARBALL="/mnt/files/github.com/databus23/helm-diff/releases/download/*/helm-diff-linux-amd64.tgz"
-    HELM_PLUGINS=$(sudo helm env | grep HELM_PLUGINS |cut -d'"' -f2)
-    sudo mkdir -p ${HELM_PLUGINS}
-    sudo tar -C ${HELM_PLUGINS} -xzf ${HELM_DIFF_TARBALL}
-  fi
-  sudo helm plugin list
-fi
 
 . ~/.envs/burrito/bin/activate
 ansible-playbook --user=${USER} --extra-vars=@vars.yml \
--- scripts/clean_k8s.sh
+++ scripts/clean_k8s.sh
@@ -6,10 +6,15 @@ CURRENT_DIR=$( dirname "$(readlink -f "$0")" )
 
 source ~/.envs/burrito/bin/activate
 OFFLINE_VARS=
+LEAP_VARS="leap_vars.yml"
+
 pushd ${CURRENT_DIR}/../
   if [ -f .offline_flag ] && \
     ${CURRENT_DIR}/offline_services.sh -s &>/dev/null; then
     OFFLINE_VARS="--extra-vars=@offline_vars.yml"
+  fi
+  if [ -f ${LEAP_VARS} ]; then
+    OFFLINE_VARS="${OFFLINE_VARS} --extra-vars=@${LEAP_VARS}"
   fi
   ansible-playbook --extra-vars=@vars.yml ${OFFLINE_VARS} \
     -b kubespray/reset.yml
