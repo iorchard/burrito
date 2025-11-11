1500eda407e622711bf3000dcb8560dc53835333
Heechul Kim <jijisa@iorchard.co.kr>
2025-11-11 15:09:29 +0900

    remove ceph-ansible collection installation. We do not need it since aster-leap is only for kubernetes upgrade.

--- prepare.sh
+++ prepare.sh
@@ -13,10 +13,6 @@ if [ x"$OPT" = x"offline" ]; then
   pip install --no-index --find-links /mnt/pypi /mnt/pypi/{pip,wheel}-*
   pip install --no-index --find-links /mnt/pypi \
                --requirement requirements.txt
-  pushd /mnt/ansible_collections
-    [ -f ceph-ansible_req.yml ] && ansible-galaxy install --force -r ceph-ansible_req.yml || :
-    [ -f pfx_req.yml ] && ansible-galaxy install --force -r pfx_req.yml || :
-  popd
 else
   sudo dnf -y install git python3 python39 python3-cryptography epel-release
   python3.9 -m venv ~/.envs/burrito
@@ -24,8 +20,6 @@ else
   python -m pip install -U pip
   python -m pip install wheel
   python -m pip install -r requirements.txt
-  ansible-galaxy install -r ceph-ansible/requirements.yml
-  ansible-galaxy install -r pfx_requirements.yml
 fi
 
 for CFG in ${CFGFILES[@]}; do
